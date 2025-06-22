d424b41bef1458f4a755633f8fec6308750c9be4
kofi-lartey <kofilartey12@gmail,com>
2025-06-22 08:32:21 +0000

    feat: enhance cart functionality with getAllcarts and deleteCartItem methods

--- controllers/cart_Controllers.js
+++ controllers/cart_Controllers.js
@@ -1,6 +1,22 @@
 import { Cart } from "../models/cart_model.js"
 import { cartSchema } from "../schemas/cart_Schema.js"
 
+
+export const getAllcarts = async(req, res) => {
+  try {
+    const userId = req.user.id;
+    if (!userId){
+      return res.status(400).json({message:'User ID is required'})
+    }
+    const carts = await Cart.find({ user: userId })
+      .populate('items')
+      .populate('user');
+    console.log('carts', carts);
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
+}
+
 export const cartStorage = async (req, res) => {
   try {
     const { error, value } = cartSchema.validate(req.body)
@@ -22,9 +38,54 @@ export const cartStorage = async (req, res) => {
 
 export const updateCartItem = async (req, res) => {
   try {
+    // the req.user.id is the id of the user that is logged in(meaning gotten from the token provided)
+    const userId = req.user.id;
+    // the req.params.id is the id of the cart that is being updated(this is placed in the url when the user wants to update a cart item eg:// /cart/:id)
+    const cartId = req.params.id;
+    // the req.body is the body of the request that is being sent to the server, it contains the advert id and the quantity of the item in the cart
+    const { advert, quantity } = req.body;
+
+    const cart = await Cart.findById(cartId);
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    // checks if the user that is trying to update the cart is the same user that created the cart(checks if the user id of the cart to be changed matches that in the token provided)
+    if (cart.user.toString() !== userId) {
+      return res.status(403).json({ message: 'Unauthorized' });
+    }
+
+    // checks for the adverts available in the cart, the ones that do not match it are saved and the ones that match are updated
+    const itemIndex = cart.items.findIndex(
+      item => item.advert.toString() === advert
+    );
+
+    // if the item index is -1, it means the item is not found in the cart....we all no that an array start from 0, so if the item index is -1, it means the item is not found in the cart
+    if (itemIndex === -1) {
+      return res.status(404).json({ message: 'Cart item not found' });
+    }
+    // if the quantity is not undefined, it means the user wants to update the quantity of the item in the cart
+    if (quantity !== undefined) {
+      cart.items[itemIndex].quantity = quantity;
+    }
+
+    // if the advert is not undefined, it means the user wants to update the advert of the item in the cart...undefined means the user does not want to update the advert of the item in the cart
+    await cart.save();
+    
+    // after saving the cart, we need to populate the items in the cart with the advert details
+    const updatedCart = await Cart.findById(cartId).populate('items.advert');
+
+    return res.status(200).json({ message: 'Cart item updated', updatedCart });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteCartItem = async (req, res) => {
+  try {
     const userId = req.user.id;
     const cartId = req.params.id;
-    const { advert, quantity } = req.body;
+    const { advert } = req.body;
 
     const cart = await Cart.findById(cartId);
     if (!cart) {
@@ -35,29 +96,18 @@ export const updateCartItem = async (req, res) => {
       return res.status(403).json({ message: 'Unauthorized' });
     }
 
-    const itemIndex = cart.items.findIndex(
-      item => item.advert.toString() === advert
-    );
-
-    if (itemIndex === -1) {
-      return res.status(404).json({ message: 'Cart item not found' });
-    }
-
-    if (quantity !== undefined) {
-      cart.items[itemIndex].quantity = quantity;
-    }
+    // checks for the adverts available in the cart, the ones that do not match it are saved and the ones that match are deleted
+    cart.items = cart.items.filter(item => item.advert.toString() !== advert);
 
     await cart.save();
 
     const updatedCart = await Cart.findById(cartId).populate('items.advert');
 
-    return res.status(200).json({ message: 'Cart item updated', updatedCart });
+    return res.status(200).json({ message: 'Cart item deleted', updatedCart });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
-
-
 
 
 export const getCartWithTotal = async (req, res) => {
--- routes/cart_route.js
+++ routes/cart_route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth.js";
-import { cartStorage, getCartWithTotal,  updateCartItem } from "../controllers/cart_Controllers.js";
+import { cartStorage, deleteCartItem, getAllcarts, getCartWithTotal,  updateCartItem } from "../controllers/cart_Controllers.js";
 
 export const cartRoute = Router();
 
 cartRoute.post('/cart', authenticate,cartStorage)
-cartRoute.post('/cart/:id', authenticate,updateCartItem)
-cartRoute.get('/cart', authenticate,getCartWithTotal)
+cartRoute.patch('/cart/:id', authenticate,updateCartItem)
+cartRoute.delete('/cart/:id', authenticate,deleteCartItem)
+cartRoute.get('/cart/total', authenticate,getCartWithTotal)
+cartRoute.get('/cart', authenticate,getAllcarts)
