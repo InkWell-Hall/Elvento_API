c317b94ea088629610e08048f333a6553b1e5a15
kofi-lartey <kofilartey12@gmail,com>
2025-06-25 11:25:30 +0000

    fix: update advert schema to change size validation from string to array

--- schemas/advert_schema.js
+++ schemas/advert_schema.js
@@ -4,7 +4,10 @@ export const advertSchema = Joi.object ({
     name: Joi.string().required(),
     description: Joi.string().required(),
     price: Joi.string().required(),
-    size: Joi.string().valid('S', 'M', 'L', 'XL','XXL').required(),
+    size: Joi.array()
+    .items(Joi.string().valid('S', 'M', 'L', 'XL', 'XXL'))
+    .min(1)
+    .required(),
     category: Joi.string().valid('matchingSet', 'top', 'kids', 'beauty').required(), 
     subCategory: Joi.string().valid('topwear', 'bottomwear', 'dresses', 'jewellery', 'cosmetics').required(),
     bestSeller: Joi.boolean(),
