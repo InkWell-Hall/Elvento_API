55d200de46df28fdbaea7d843d9bf2f3660f058b
kofi-lartey <kofilartey12@gmail,com>
2025-06-25 16:18:00 +0000

    fix: clean up advert schema formatting and remove images field

--- schemas/advert_schema.js
+++ schemas/advert_schema.js
@@ -1,19 +1,15 @@
 import Joi from 'joi'
 
-export const advertSchema = Joi.object ({
+export const advertSchema = Joi.object({
     name: Joi.string().required(),
     description: Joi.string().required(),
     price: Joi.number().required(),
     size: Joi.array()
-    .items(Joi.string().valid('S', 'M', 'L', 'XL', 'XXL'))
-    .min(1)
-    .required(),
-    category: Joi.string().valid('matchingSet', 'top', 'kids', 'beauty').required(), 
+        .items(Joi.string().valid('S', 'M', 'L', 'XL', 'XXL'))
+        .min(1)
+        .required(),
+    category: Joi.string().valid('matchingSet', 'top', 'kids', 'beauty').required(),
     subCategory: Joi.string().valid('topwear', 'bottomwear', 'dresses', 'jewellery', 'cosmetics').required(),
     bestSeller: Joi.boolean(),
     date: Joi.number(),
-    images: Joi.array()
-    .items(Joi.string().uri().required()) // if each string is a URL
-    .min(1)
-    .required(),
 });
